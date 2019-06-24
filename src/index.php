3cde1b730d71af747e1387a5efb6bc5ba9111c87
rd7.rodrigo <rd7.rodrigo@gmail.com>
2019-06-24 15:55:29 -0300

    ajuste no caminho do autoload.php

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
  */
 
 // require_once __DIR__ . '/../../../vendor/autoload.php'; //Develop
-require_once __DIR__ . '/../../autoload.php';
+require_once __DIR__ . '/../../../autoload.php';
 //dentro de vendor/rodrigodesouza/laravel-autodeploy
 
 
