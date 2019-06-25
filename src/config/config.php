d87a0194550ede3f86c8ab1595ff30fccf445d37
rd7.rodrigo <rd7.rodrigo@gmail.com>
2019-06-25 15:24:36 -0300

    novas funcoes e atualizacoes

--- src/config/config.php
+++ src/config/config.php
@@ -2,14 +2,19 @@
 
 return [
     'name' => 'Laravel Autodeploy',
+
     'branch' => env('AUTODEPLOY', 'production'),
+
     'deploy_de' => env('DEPLOY_DE', 'master'),
+
     'deploy_para' => env('DEPLOY_PARA', 'production'),
+
     'errors_log' => [
         'CONFLICT',
         'error: ',
         'fatal'
     ],
+
     'commands' => [
         'local' => [
             'git add . && git commit -m "{commit}"',
@@ -26,7 +31,17 @@ return [
             'git reset --hard origin/{branch}'
         ]
     ],
-    'folder_git' => '../', //base_path(),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Folder Git
+    |--------------------------------------------------------------------------
+    | Não usar helpers como: base_path(),
+    | "../" subir a partir da pasta public/www/public_html
+    */
+    'folder_git' => '../',
+
     'desktop_notification' => true,
+
     'save_request_body' => true
 ];
